f12ce8dc4078713fb7101b6ace0b4621ac6d573f
nmquang1416 <nmquang1416@gmail.com>
2024-03-13 15:24:47 +0700

    update

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import logo from '../Assets/Img/logo-Ci.png';
+
+const Navbar = () => {
+
+    const [menu,setMenu] = useState("Home")
+    
+
+    return (
+        <div className="container-nav">
+            <div className='navbar'>
+                <div className='nav-logo'>
+                    <img src={logo} alt="logo-cellinfo" />
+                </div>
+                <ul className='nav-menu'>
+                    <li onClick = {() => {setMenu("Home")}}>Home{menu==="Home"?<hr/>:<></>}</li>
+                    <li onClick = {() => {setMenu("Abouts us")}}>About us{menu==="About us"?<hr/>:<></>}</li>
+                    <li onClick = {() => {setMenu("Product")}}>Product{menu==="Product"?<hr/>:<></>}</li>
+                    <li onClick = {() => {setMenu("Service")}}>Service{menu==="Service"?<hr/>:<></>}</li>
+                    <li onClick = {() => {setMenu("Compare")}}>Compare {menu==="Compare"?<hr/>:<></>}</li>
+                </ul>
+                <div className="nav-input-search">
+                    <form>
+                        <input type="text" placeholder='ex:iphone 15 pro vip'/>
+                        <button className='submit'>
+                            <i class="bi bi-search"></i>
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
