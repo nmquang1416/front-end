de42ab3cbddb273c24b50b779bd3afab92d8d40e
nmquang1416 <nmquang1416@gmail.com>
2024-03-18 22:05:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { Shop } from "./Pages/Shop";
-import { Home } from "./Pages/PageOfCellinfo/Home";
+import { Home } from "./PageOfCellinfo/ProductOfCellinfo";
 import { ShopCategory } from "./Pages/ShopCategory";
-import { ProductOfCellinfo } from "./Pages/PageOfCellinfo/ProductOfCellinfo";
+import { ProductOfCellinfo } from "./PageOfCellinfo/ProductOfCellinfo";
+import  Hero  from "./Components/Hero/Hero";
 
 function App() {
   return (
@@ -17,11 +18,13 @@ function App() {
             <Route path='/ProductOfCellinfo' element={<ShopCategory category="ProductOfCellinfo"/>}/>
             <Route path='/Service' element={<ShopCategory category="Service"/>}/>
             <Route path='/Compare' element={<ShopCategory category="Compare"/>}/>
-            <Route path="/ProductOfCellinfo" element={<ProductOfCellinfo/>}>
+            <Route path='/ProductOfCellinfo' element={<ProductOfCellinfo/>}>
               <Route path =':productId' element={<ProductOfCellinfo/>}/>
             </Route>
           </Routes>
       </BrowserRouter>
+
+      <Hero/>
     </div>
   );
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Hero.css';
+import background_hero from '../Assets/Img/Banner-hero-3.png';
+import iphone15 from '../Assets/Img/Iphone.png';
+
+
+const Hero = () => {
+    return (
+        <div className='hero' style={{ backgroundImage: `url(${background_hero})` }}>
+            <div className="hero-content">
+                <div className="hero-left">
+                    <h2 className='hero-title'>IPHONE 15 PRO VIP</h2>
+                    <p className="sub-title">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </div>
+                <div className="hero-right">
+                    <img src={iphone15} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
+
+
+
+
