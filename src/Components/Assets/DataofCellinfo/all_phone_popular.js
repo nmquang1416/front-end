c38f134a33acd5ae0f33b9fe88fce709905214eb
nmquang1416 <nmquang1416@gmail.com>
2024-03-19 17:35:42 +0700

    Update all_phone_popular.js

--- src/Components/Assets/DataofCellinfo/all_phone_popular.js
+++ src/Components/Assets/DataofCellinfo/all_phone_popular.js
@@ -1,8 +1,20 @@
 let all_phone_popular = [
     {
         banner_id: 1,
-        banner_title:"Glaxy Z Fold",
-        sub
-        
+        title:"Glaxy Z Fold",
+        sub_title:"A powerful and flexible smartphone like a computer right in your pocket",
+        button: "View more",
+    },
+    {
+        banner_id: 1,
+        title:"Glaxy Z Fold",
+        sub_title:"A powerful and flexible smartphone like a computer right in your pocket",
+        button: "View more",
+    },
+    {
+        banner_id: 1,
+        title:"Glaxy Z Fold",
+        sub_title:"A powerful and flexible smartphone like a computer right in your pocket",
+        button: "View more",
     }
 ]
