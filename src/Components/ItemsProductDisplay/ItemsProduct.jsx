068e3d82a6da10ec06ed4efe2f50dcb1bbecf693
nmquang1416 <nmquang1416@gmail.com>
2024-03-29 00:41:34 +0700

    update

--- src/Components/ItemsProductDisplay/ItemsProduct.jsx
+++ src/Components/ItemsProductDisplay/ItemsProduct.jsx
@@ -13,9 +13,8 @@ const ItemsProduct = (props) => {
         <p className="title-card-product">{props.name} </p>
         <p className="sub-title-card-product">{props.Description}</p>
         <div className="button-product-card">
-          <button className="Button-order">{props.button_1}</button>
+          <Link to={"/Compare/"}><button className="Button-order">{props.button_1}</button></Link>
           <Link to={"/ProductOfCellinfo/"+props.id}><button className="Button-more">{props.button_2}</button></Link>
-          
         </div>
       </div>
     </div>
--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -7,20 +7,6 @@ import ItemsProductDisplay from '../ItemsProductDisplay/ItemsProduct';
 import all_product_cellinfo from '../Assets/DataofCellinfo/all_product_cellinfo';
 
 
-
-
-export function GetId() {
-  return (
-    <div>
-      {all_product_cellinfo.map(Product_id =>(
-        <p>{Product_id.id}</p>
-    ))}
-    </div>
-  )
-}
-
-
-
 const ProductDisplay = () => {
     return (
         <div className='Product-Display' >
