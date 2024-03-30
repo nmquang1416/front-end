5724c5954cabf277ab4ef980246efb9025632806
nmquang1416 <nmquang1416@gmail.com>
2024-03-31 00:40:23 +0700

    update

--- src/Components/Assets/DataofCellinfo/all_product_cellinfo.js
+++ src/Components/Assets/DataofCellinfo/all_product_cellinfo.js
@@ -1,4 +1,13 @@
-import img_1 from "../Img/iphone-items-product.png";
+import img_1 from "../Img/Thumbnail_Product/iphone-13-pro-thumb.png";
+import img_2 from "../Img/Thumbnail_Product/iphone-12-pro-thumb.png";
+import img_3 from "../Img/Thumbnail_Product/samsung-s24-thumb.png";
+import img_4 from "../Img/Thumbnail_Product/xiaomi-14-thumb.png";
+import img_5 from "../Img/Thumbnail_Product/samsung-s23-thumb.png";
+import img_6 from "../Img/Thumbnail_Product/samsung-galaxy-a55.png";
+import img_7 from "../Img/Thumbnail_Product/ipad-pro-thumb.png";
+import img_8 from "../Img/Thumbnail_Product/xiaomi-redmi-14-thumb.png";
+import img_9 from "../Img/Thumbnail_Product/iphone-14-thumb.png";
+
 
 // // product can lay = all_product_cellinfo.filter((item)=>{
 // //     return item.id == product_id
@@ -18,7 +27,7 @@ let all_product_cellinfo = [
         name:"Iphone 12 Pro",
         Description: "At the end of 2020, the iPhone 12 set of 4 was released with many improvements. After that, all attention focused on the next product - iPhone 13. ", 
         categories: "Iphone",
-        image: img_1,
+        image: img_2,
         button_1: "Compare",
         button_2: "View more",
     },
@@ -27,7 +36,7 @@ let all_product_cellinfo = [
         name:"Samsung S24 Ultra",
         Description: "Samsung S24 Ultra is the top smartphone product opening in 2024 from Samsung with powerful Snapdragon 8 Gen 3 For Galaxy chip, future technology Galaxy AI", 
         categories: "Samsung",
-        image: img_1, 
+        image: img_3, 
         button_1: "Compare",
         button_2: "View more",
     },
@@ -36,7 +45,7 @@ let all_product_cellinfo = [
         name:"Xiaomi 14 - 256GB",
         Description: "Xiaomi 14 5G has a 6.36-inch OLED screen, along with a Qualcomm Snapdragon 8 Gen 3 processor, creating a super powerful configuration for users.", 
         categories: "Xiaomi",
-        image: img_1,
+        image: img_4,
         button_1: "Compare",
         button_2: "View more",
     },
@@ -45,7 +54,7 @@ let all_product_cellinfo = [
         name:"Samsung S23 Ultra",
         Description: "After the successful landing of Samsung Galaxy S22, the next Flagship phones - Samsung Galaxy S23 Ultra phones are highly sought after by Samfans.", 
         categories: "Samsung",
-        image: img_1,
+        image: img_5,
         button_1: "Compare",
         button_2: "View more",
     },
@@ -54,7 +63,7 @@ let all_product_cellinfo = [
         name:"Samsung Galaxy A55",
         Description: "Samsung Galaxy A55 has a luxurious, modern design with a 6.6-inch wide screen, Super AMOLED panel and Full HD+ resolution for smooth, sharp images.", 
         categories: "Samsung",
-        image: img_1,
+        image: img_6,
         button_1: "Compare", 
         button_2: "View more",
     },
@@ -63,7 +72,7 @@ let all_product_cellinfo = [
         name:"iPad Pro 11 inch 2022",
         Description: "Launched with many improvements and powerful performance, iPad Pro 2022 brings many exciting experiences to users. Launched with many improvements ", 
         categories: "Iphone",
-        image: img_1,
+        image: img_7,
         button_1: "Compare",
         button_2: "View more",
     },
@@ -72,7 +81,7 @@ let all_product_cellinfo = [
         name:"Xiaomi Redmi Note 13 ",
         Description: "Redmi Note 13 possesses notable upgrades with Snapdragon 685 chip, Mali-G57 GPU and 6GB of RAM to improve performance by up to 25% and graphics display.", 
         categories: "Iphone",
-        image: img_1,
+        image: img_8,
         button_1: "Compare",
         button_2: "View more",
     },
@@ -81,7 +90,7 @@ let all_product_cellinfo = [
         name:"iPhone 14 Pro Max ",
         Description: "iPhone 14 Pro Max has a large screen improvement compared to iPhone 13 Pro Max. The difference between the iPhone 14 Pro Max 256GB version and the standard 128GB.", 
         categories: "Iphone",
-        image: img_1,
+        image: img_9,
         button_1: "Compare",
         button_2: "View more",
     },
